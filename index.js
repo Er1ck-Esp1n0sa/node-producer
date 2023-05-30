26079f0b7d3270b6c989c35b11f1d030d2aaf08d
Er1ck-Esp1n0sa <zs20006728@estudiantes.uv.mx>
2023-05-30 11:12:20 -0600

    update express

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
   clientId: 'my-app',
-  brokers: [
-	  "my-kafka-0.my-kafka-headless.er1ck-esp1n0sa.svc.cluster.local:9092"
+  brokers: ["my-kafka-0.my-kafka-headless.er1ck-esp1n0sa.svc.cluster.local:9092"
+    //'localhost:9092'
+	  //'my-kafka-0.my-kafka-headless.kafka-adsoftsito.svc.cluster.local:9092'
 	  ]
 });
 
@@ -21,7 +22,57 @@ app.get('/', (req, res, next) => {
   res.send('kafka api - Erick Juarez Espinosa');
 });
 
-const run = async (username) => {
+/*Para las reacciones*/
+const runreaction = async (username, publication_id, reaction)=>{
+  await producer.connect()
+  await producer.send({
+    topic: 'reactions',
+    messages:[{
+      'value': `{"name":"${username}",
+                 "publication":"${publication_id}",
+                 "reaction":"${reaction}"}` 
+    }],
+  })
+  await producer.disconnect()
+}
+
+app.get('/reaction', (req, res, next) => {
+  const username = req.query.name;
+  const publication_id = req.query.publication_id;
+  const reaction = req.query.reaction;
+  res.send({ 'name' : username,
+             'publication' : publication_id,
+             'reaction': reaction} );
+  runreaction(username, publication_id, reaction).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+/*Para los comentarios*/
+const runcomments = async (username, publication_id, comment)=>{
+  await producer.connect()
+  await producer.send({
+    topic: 'comments',
+    messages:[{
+      'value': `{"name":"${username}",
+                 "publication":"${publication_id}",
+                 "comment":"${comment}"}` 
+    }],
+  })
+  await producer.disconnect()
+}
+
+app.get('/comment', (req, res, next) => {
+  const username = req.query.name;
+  const publication_id = req.query.publication_id;
+  const comment = req.query.comment;
+  res.send({ 'name' : username,
+             'publication' : publication_id,
+             'comment': comment} );
+  runcomments(username, publication_id, comment).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+/*const run = async (username) => {
 
     await producer.connect()
 //    await producer.send()
@@ -41,7 +92,7 @@ app.get('/like', (req, res, next) => {
   res.send({ 'name' : username } );
   run(username).catch(e => console.error(`[example/producer] ${e.message}`, e))
 
-});
+});*/
 
 app.listen(port,  () => 
 	console.log('listening on port ' + port
